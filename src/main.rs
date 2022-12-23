61d8e1d0f89ad2661a984058039c6e864702014a
eddde <50966754+eddde@users.noreply.github.com>
2022-12-23 20:20:21 +0100

    Recording moves in player vs computer matches

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ mod types;
 use ansi_term::Colour::{Red, White, RGB};
 use cpu::get_all_moves;
 use moves::*;
+use std::fmt::format;
+use std::fs;
 use std::io::{stdin, Write};
 use types::*;
 
 fn main() {
     clear_screen();
     let board = Board::new();
-    arrow_print("Welcome to C-Chess!", true);
+    arrow_print("Welcome to E-Chess!", true);
     arrow_print("Input 'exit' to exit the application at anytime.", false);
     arrow_print(
         "What do you want to play?\n\n(1) Local Multiplayer\n(2) Computer vs Computer\n(3) Singleplayer vs Computer\n",
@@ -39,14 +41,14 @@ fn main() {
 fn mp_game_loop(mut board: Board) {
     clear_draw(board, true);
     loop {
-        new_turn(&mut board, true, true);
+        new_turn(&mut board, true, true, None);
         if let Some(winner) = check_for_mates(board) {
             clear_draw(board, true);
             arrow_print(&format!("{} Wins!", winner.ctos()), true);
             break;
         }
 
-        new_turn(&mut board, false, true);
+        new_turn(&mut board, false, true, None);
         if let Some(winner) = check_for_mates(board) {
             clear_draw(board, false);
             arrow_print(&format!("{} Wins!", winner.ctos()), true);
@@ -267,6 +269,67 @@ fn sp_game_loop(mut board: Board) {
         break;
     }
 
+    clear_screen();
+    arrow_print("Should game be recorded? y/n", true);
+
+    let should_record: bool;
+    loop {
+        let mut input = String::new();
+        print!(">>> ");
+        std::io::stdout().flush().unwrap();
+        stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_lowercase();
+        match input.as_str() {
+            "y" => should_record = true,
+            "n" => should_record = false,
+            _ => {println!("Invalid input!"); continue;}
+        }
+
+        break;
+    }
+
+    let mut file: Option<std::fs::File>;
+
+    if should_record {
+        clear_screen();
+
+        let problematic_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', '\u{1F}'];
+        let file_name: String;
+        loop {
+            arrow_print("What should the filename be?", true);
+            arrow_print("Notice that files with the same name as previous ones will be truncated", false);
+            let mut input = String::new();
+            print!(">>> ");
+            std::io::stdout().flush().unwrap();
+            stdin().read_line(&mut input).unwrap();
+            let input = input.trim().to_lowercase();
+
+            if problematic_chars.iter().any(|&v| input.contains(v)) {
+                clear_screen();
+                println!("Cannot use characters: ");
+                for c in problematic_chars.iter() {
+                    print!("{}, ", c);
+                } 
+                println!(" in filename");
+                continue;
+            } else {
+                file_name = input;
+            }
+
+            break;
+        }
+        let file_path = (&file_name as &str).to_owned() + ".edvard";
+        fs::File::create(&file_path);
+        file = Some(fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&file_path)
+        .unwrap());
+
+    } else {
+        file = None;
+    }
+
     for _ in 0..2 {
         if let Some(winner) = check_for_mates(board) {
             clear_draw(board, false);
@@ -277,11 +340,12 @@ fn sp_game_loop(mut board: Board) {
         let mut all_moves = get_all_moves(board, true);
         all_moves.retain(|x| x.1.len() != 0);
         let move_ = &all_moves[fastrand::usize(0..all_moves.len())];
+        let move_to = move_.1[fastrand::usize(..move_.1.len())];
 
         match move_piece(
             &mut board,
             move_.0,
-            move_.1[fastrand::usize(..move_.1.len())],
+            move_to,
             true,
         ) {
             Err(e) => {
@@ -290,6 +354,10 @@ fn sp_game_loop(mut board: Board) {
             }
 
             Ok(_) => {
+                match file {
+                    Some(ref mut file) => writeln!(file, "{:?}:{:?}", move_.0, move_to).unwrap(),
+                    None => {},
+                }
                 clear_draw(board, false);
                 arrow_print("Computer is doing one of it's 2 random moves...", true)
             }
@@ -301,7 +369,7 @@ fn sp_game_loop(mut board: Board) {
             break;
         }
 
-        new_turn(&mut board, false, true);
+        new_turn(&mut board, false, true, file.as_mut());
 
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }
@@ -323,7 +391,12 @@ fn sp_game_loop(mut board: Board) {
                 input_error(e);
             }
 
-            Ok(_) => {}
+            Ok(_) => {
+                match file {
+                    Some(ref mut file) => writeln!(file, "{:?}:{:?}", move_.0, move_.1).unwrap(),
+                    None => {},
+                }
+            }
         }
 
         if let Some(winner) = check_for_mates(board) {
@@ -342,11 +415,11 @@ fn sp_game_loop(mut board: Board) {
             black_start.elapsed()
         );
 
-        new_turn(&mut board, false, false);
+        new_turn(&mut board, false, false, file.as_mut());
     }
 }
 
-fn new_turn(board: &mut Board, is_white: bool, clear: bool) {
+fn new_turn(board: &mut Board, is_white: bool, clear: bool, write_file: Option<&mut std::fs::File>) {
     loop {
         if clear {
             clear_draw(*board, is_white)
@@ -368,6 +441,11 @@ fn new_turn(board: &mut Board, is_white: bool, clear: bool) {
                 }
 
                 Ok(_) => {
+                    match write_file {
+                        Some(write_file) => writeln!(write_file, "{:?}:{:?}", from, to).unwrap(),
+                        None => {},
+                    }
+                    //write_file.write_all(from + "," + to + "\n");
                     clear_draw(*board, is_white);
                     return;
                 }
